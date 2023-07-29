aba4abec7fb81d5698f49805f06bb855a27e05bd
TomsLielkalns <lielkalnstoms@gmail.com>
2023-07-29 04:23:22 +0300

    css upgrade

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ export const PageLayout = (props: PropsWithChildren) => {
   return (
     <>
       <main className="flex justify-center">
-        <div className="no-scrollbar h-screen w-full overflow-y-scroll border-x border-slate-300 md:max-w-2xl">
+        <div className="no-scrollbar h-screen w-full overflow-y-scroll border-x border-slate-300 bg-gray-800 drop-shadow-2xl md:max-w-2xl">
           {children}
         </div>
       </main>
